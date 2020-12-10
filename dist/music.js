03302a11c8c12d69fdfce1a35fca4c279233762b
oycy0306 <oycy006@hotmail.com>
2020-12-10 22:16:33 +0800

    Site updated: 2020-12-10 22:16:32

--- dist/music.js
+++ dist/music.js
@@ -11,7 +11,6 @@ const ap = new APlayer({
         artist: 'Kurt Hugo Schneider',
         url: 'http://dl.stream.qqmusic.qq.com/C100002XlXGB3f8uZN.m4a?fromtag=38',
         cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000005TTDJ3Q34uK_1.jpg?max_age=2592000',
-      },
-
+      }
     ]
 });
