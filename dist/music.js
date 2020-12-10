0f3f7d0ed21d0b103f8748212fd982513e15a295
oycy0306 <oycy006@hotmail.com>
2020-12-10 22:13:40 +0800

    Site updated: 2020-12-10 22:13:39

--- dist/music.js
+++ dist/music.js
@@ -7,64 +7,11 @@ const ap = new APlayer({
 	volume: 0.2,
     audio: [
       {
-        name: "心拍数#0822",
-        artist: 'Akie秋绘',
-        url: 'http://www.ytmp3.cn/down/57734.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M0000024Yoan3vZATh.jpg',
+        name: "Champions（英雄联盟S10应援曲）",
+        artist: 'Kurt Hugo Schneider',
+        url: 'http://dl.stream.qqmusic.qq.com/C100002XlXGB3f8uZN.m4a?fromtag=38',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000005TTDJ3Q34uK_1.jpg?max_age=2592000',
       },
-      {
-        name: 'Gorgeous',
-        artist: 'Taylor Swift',
-        url: 'http://www.ytmp3.cn/down/49722.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003OK4yP2MBOip.jpg',
-      },
-	  {
-        name: 'オレンジ (Orange)',
-        artist: 'Seven Oops',
-        url: 'http://www.ytmp3.cn/down/36234.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000004KzDAJ3nAmdu.jpg',
-      },
-	  {
-        name: 'カワキヲアメク',
-        artist: '美波',
-        url: 'http://www.ytmp3.cn/down/58673.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M00000386Sll4AfJXk.jpg',
-      },
-	  {
-        name: 'Venom (Clean)',
-        artist: 'Eminem',
-        url: 'http://www.ytmp3.cn/down/56147.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003YRAq21EltzO.jpg',
-      },
-	  {
-        name: 'アイロニ (反语) (カバー)',
-        artist: 'majiko (まじ娘)',
-        url: 'http://www.ytmp3.cn/down/60271.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000002FzKSY0FcypL.jpg',
-      },
-	  {
-        name: 'LOSER',
-        artist: '米津玄師 (よねづ けんし)',
-        url: 'http://www.ytmp3.cn/down/49909.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000002kl9Hs4WaKFO.jpg',
-      },
-	  {
-        name: 'I Knew You Were Trouble',
-        artist: 'Taylor Swift',
-        url: 'http://www.ytmp3.cn/down/51501.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003XgDKL12X3J1.jpg',
-      },
-	  {
-        name: '打上花火',
-        artist: 'DAOKO (ダヲコ),米津玄師 (よねづ けんし)',
-        url: 'http://www.ytmp3.cn/down/50434.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000004HjUxU1o3HXf.jpg',
-      },
-	  {
-        name: 'Despacito (Remix)',
-        artist: 'Luis Fonsi,Daddy Yankee,Justin Bieber',
-        url: 'http://www.ytmp3.cn/down/49984.mp3',
-        cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003z7S4p3J92dA.jpg',
-      }
+
     ]
 });
