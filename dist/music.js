4eea747192a5d06c1657f26d9a8b120095aecf69
oycy0306 <oycy006@hotmail.com>
2021-08-15 13:51:49 +0800

    Site updated: 2021-08-15 13:51:45

--- dist/music.js
+++ dist/music.js
@@ -2,15 +2,90 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
-	loop: 'all',
-	order: 'random',
-	volume: 0.2,
     audio: [
       {
-        name: "Champions（英雄联盟S10应援曲）",
-        artist: 'Kurt Hugo Schneider',
-        url: 'http://dl.stream.qqmusic.qq.com/C100002XlXGB3f8uZN.m4a?fromtag=38',
-        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M0000005TTDJ3Q34uK_1.jpg?max_age=2592000',
+        name: "ラブレター",
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75493.mp3',
+        cover: 'https://img4.kuwo.cn/star/albumcover/500/16/0/1308868856.jpg',
+      },
+      {
+        name: '怪物',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75492.mp3',
+        cover: 'https://img1.kuwo.cn/star/albumcover/500/83/4/2988322061.jpg',
+      },
+      {
+        name: '夜に駆ける',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75494.mp3',
+        cover: 'https://img4.kuwo.cn/star/albumcover/500/16/0/1308868856.jpg',
+      },
+      {
+        name: 'たぶん',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75495.mp3',
+        cover: 'https://img1.kuwo.cn/star/albumcover/500/49/2/1014824909.jpg',
+      },
+      {
+        name: '群青',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75496.mp3',
+        cover: 'https://img1.kuwo.cn/star/albumcover/500/49/2/1014824909.jpg',
+      },
+      {
+        name: '三原色',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75497.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/1/45/1383434958.jpg',
+      },
+      {
+        name: '優しい彗星',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75498.mp3',
+        cover: 'https://img4.kuwo.cn/star/albumcover/500/62/20/2764088116.jpg',
+      },
+      {
+        name: 'もう少しだけ',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75499.mp3',
+        cover: 'https://img4.kuwo.cn/star/albumcover/500/27/13/3338509315.jpg',
+      },
+      {
+        name: 'ハルジオン',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75500.mp3',
+        cover: 'https://img4.kuwo.cn/star/albumcover/500/27/13/3338509315.jpg',
+      },
+      {
+        name: 'アンコール',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75501.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/57/79/436785350.jpg',
+      },
+      {
+        name: 'ハルカ',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75502.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/26/25/3668575491.jpg',
+      },
+      {
+        name: 'あの夢をなぞって	',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75503.mp3',
+        cover: 'https://img3.kuwo.cn/star/albumcover/500/53/10/423466712.jpg',
+      },
+      {
+        name: 'Prologue',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75504.mp3',
+        cover: 'https://img3.kuwo.cn/star/albumcover/500/53/10/423466712.jpg',
+      },
+      {
+        name: 'Epilogue',
+        artist: 'YOASOBI',
+        url: 'https://www.ytmp3.cn/down/75505.mp3',
+        cover: 'https://img2.kuwo.cn/star/albumcover/500/57/79/436785350.jpg',
       }
     ]
 });
